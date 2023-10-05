ecf62580b0fe44b4c0fd577870b07183d388e623
divanshu <gdivanshu6@gmail.com>
2023-10-05 09:24:48 +0530

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  let [content, setcontent] = useState("");
+  let [list, setlist] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='todo_app'>Todo List</h1>
+      <div className="nav_todo">
+        <input type="text" className="content" id="content" value={content} onChange={(e) => setcontent(e.target.value)}/>
+        <button className='add_ele' onClick={() => {
+            if (content != '') {
+              setlist([...list, content])
+              content = '';
+              setcontent(content);
+            }
+          }}>Add</button>
+      </div>
+      <div className='item_container'>
+        {
+          list.map((e, i) => {
+            return (
+              <div className='item'>
+                <p key={i}>{e}</p>
+                <button onClick={() => setlist(list.filter((ele, ile) => ile != i))}>delete</button>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
